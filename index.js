a2f14c6fce1707511da402dbb1e52590d3d5d9a4
CMRicardo <ricardocorrales84@gmail.com>
2023-12-14 12:23:50 -0600

    feat: :heavy_plus_sign: Add cors

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import { dbConnection } from './database/config.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
+app.use(cors())
 dbConnection()
 
 app.get('/', (req, res) => {
   res.json({ message: '👋 Hello world!' })
 })
 
-// ricardocorrales84
-// 893B6PfUPbYwkney
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
